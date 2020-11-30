e55ee1152d336e0847425606ad495db73ce77085
Maciej Ra <maciejrajcic@gmail.com>
2020-11-30 14:50:35 +0100

    Adding calculator class

---
+++ src/main/java/com/kodillla/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodillla.calculator;
+
+public class Calculator {
+
+    public void someMethod(){
+        System.out.println("it works");
+    }
+
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+    }
+}
